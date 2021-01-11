16ff30b808880275659912d12e43d4791c96c38f
Helen Lu <helenlu@Helens-MacBook-Pro.local>
2021-01-11 10:45:56 -0500

    added route file

---
+++ src/routes/integration_routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const transformationController = require('../controllers/integration-controller.js');
+const authenticationMiddleware = require('../middlewares/authentication').authenticationMiddleware;
+
+router.post('/transformation/transform', authenticationMiddleware, transformationController.transformToMondayColumn);
+router.post('/transformation/types', authenticationMiddleware, transformationController.getTransformationTypes);
+router.post('/transformation/subscribe', authenticationMiddleware, transformationController.subscribe);
+router.post('/transformation/unsubscribe', authenticationMiddleware, transformationController.unsubscribe);
+router.post('/transformation/email', authenticationMiddleware, transformationController.populateTextColumnFromPeopleColumn);
+
+module.exports = router;
